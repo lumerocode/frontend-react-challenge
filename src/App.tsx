84285da5547f255b559056cec9fbfa9b350c44e2
lumerocode <luismelendez0894@gmail.com>
2026-05-28 01:04:16 -0500

    feat: initialize project, folder structure and tailwind v4 configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-rimac-lightBg">
+      <p className="text-rimac-red font-sans font-regular text-xl tracking-wide">
+        ¡Mensaje de prueba!
+      </p>
+    </div>
+  )
+}
+
+export default App
